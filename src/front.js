5a2ae1a7b1ac2ed079f483fcbafb59445d80fa4a
Rajan kumar <rajan.kumar006000@gmail.com>
2019-07-06 17:02:42 +0530

    First trial

---
+++ src/front.js
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta name="referrer" content="origin">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js" charset="UTF-8">
+</script>
+</head>
+<body>
+<b>Login</b><br>
+<form method="post" action="comment">
+<table border="1"><tr><td>username:</td><td><input type="text" name="acct" value="qwfdqe" size="20" autocorrect="off" spellcheck="false" autocapitalize="off" autofocus="true"></td></tr><tr><td>password:</td><td><input type="password" name="pw" value="bvhg5666jk" size="20"></td></tr></table><br>
+<input type="submit" value="login"></form>
+<a href="forgot">Forgot your password?</a><br><br>
+<b>Create the Account</b><br>
+<form method="post" action="comment">
+<input type="hidden" name="creating" value="t">
+<table border="0"><tr><td>username:</td><td>
+<input type="text" name="Account" value="Account" size="20" autocorrect="off" spellcheck="false" autocapitalize="off"></td></tr><tr><td>password:</td><td><input type="password" name="pw" value="bvhg5666jk" size="20"></td></tr></table><br>
+<input type="submit" value="create account">
+</form> 
+</body>
+</html>
